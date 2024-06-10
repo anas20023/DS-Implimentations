5ae257f736a5ce87b0bf4b46798bc5e1754629b0
anas20023 <yorder07@gmail.com>
2024-06-10 19:23:37 +0600

    chore: Refactor stack.cpp to initialize array elements to zero in the Stack constructor

--- stack.cpp
+++ stack.cpp
@@ -1,13 +1,22 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
-class Stack {
-    vector<int>v;
-public:
+class Stack
+{
+    int x;
 
+public:
+    Stack()
+    {
+        cin >> x;
+        int arr[x];
+        for (int i = 0; i < x; i++)
+        {
+            arr[i] = 0;
+        }
+    }
 };
 
 int main()
 {
-
 }
