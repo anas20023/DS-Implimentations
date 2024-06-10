3d5e7aa2e82d8526b5a20504ca3412bb10f1a528
anas20023 <yorder07@gmail.com>
2024-06-10 19:52:37 +0600

    Refactor Stack class to include a dynamic array for storing elements

--- stack.cpp
+++ stack.cpp
@@ -3,13 +3,14 @@ using namespace std;
 
 class Stack
 {
-    int x;
+    int x,tp;
+    int arr[1000];
 
 public:
     Stack()
     {
+        tp=-1;
         cin >> x;
-        int arr[x];
         for (int i = 0; i < x; i++)
         {
             arr[i] = 0;
@@ -17,11 +18,27 @@ public:
     }
     void push(int n)
     {
+        tp++;
+        arr[tp]=n;
 
+    }
+    void display()
+    {
+        for(int i =x-1;i>=0;i--)
+        {
+            cout<<arr[i]<<endl;
+        }
     }
 };
 
 int main()
 {
     Stack s1;
+    while(1)
+    {
+        int m;
+        cin>>m;
+        s1.push(m);
+        s1.display();
+    }
 }
