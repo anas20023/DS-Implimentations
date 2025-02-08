379e96c596391581ee92f18b23c785464a06745f
anas20023 <anasibnebelal400@gmail.com>
2025-02-08 18:47:44 +0600

    Implement Fractional Bisection algorithm: add logic for finding roots using binary search

---
+++ FractionalBisection.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int n;
+bool isOK(double x){
+    if(x*x<n) return 0;
+    else return 1;
+}
+int main()
+{
+    cin>>n;
+    double l=0,r=n+1;
+    while(r-l > 0.0000000001){  /// l < dile INF loop hobe .... So, r-l >0.0000000001
+        double mid=(l+r)/2;
+        if(isOK(mid)==0) l=mid;
+        else r=mid;
+    }
+    cout<<l<<endl;
+    return 0;
+}
