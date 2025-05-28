f2f4d8e637dad00dfe53547264c05b6c9ee0bc07
Anas Ibn Belal <98760910+anas20023@users.noreply.github.com>
2025-05-29 00:22:08 +0600

    Add files via upload
    dijkstra using priority Q

---
+++ Shortest_Routes_I.cpp
@@ -0,0 +1,214 @@
+//onAC(ALPHA) =>{Target Next ICPC ^-^}           
+//  ^-^      ^-^       ^-^    ^-^     ^-^  
+#include <bits/stdc++.h>
+#include <ext/pb_ds/assoc_container.hpp> 
+#include <ext/pb_ds/tree_policy.hpp> 
+using namespace std;
+using namespace __gnu_pbds;
+
+
+template <typename T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int, int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef double dl;
+
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+#define endl '\n'
+#define all(a) (a).begin(), (a).end()
+#define sz(x) (int)x.size()
+#define mid(l, r) ((r + l) / 2)
+#define left(node) (node * 2)
+#define right(node) (node * 2 + 1)
+#define mx_int_prime 999999937
+#define arrin(v,n) for(int i=0;i<n;i++)cin>>v[i];
+const double PI = (2.0 * acos(0.0));
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+
+#define mem(a, b) memset(a, b, sizeof(a))
+#define gcd(a, b) __gcd(a, b)
+#define lcm(a, b) (a * (b / gcd(a, b)))
+#define sqr(a) ((a) * (a))
+
+#define optimize()                \
+    ios_base::sync_with_stdio(0); \
+    cin.tie(0);                   \
+    cout.tie(0);
+#define fraction()                \
+    cout.unsetf(ios::floatfield); \
+    cout.precision(10);           \
+    cout.setf(ios::fixed, ios::floatfield);
+#define file()                        \
+    freopen("inputf.in", "r", stdin); \
+    freopen("outpuft.out", "w", stdout);
+
+typedef vector<int>::iterator vit;
+typedef set<int>::iterator sit;
+
+int dx[] = {0, 0, +1, -1};
+int dy[] = {+1, -1, 0, 0};
+// int dx[] = {+1, 0, -1, 0, +1, +1, -1, -1};
+// int dy[] = {0, +1, 0, -1, +1, -1, +1, -1};
+
+template <typename F, typename S>
+ostream &operator<<(ostream &os, const pair<F, S> &p)
+{
+    return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const vector<T> &v)
+{
+    os << "{";
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if (it != v.begin())
+            os << ", ";
+        os << *it;
+    }
+    return os << "}";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const set<T> &v)
+{
+    os << "[";
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if (it != v.begin())
+            os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template <typename T>
+ostream &operator<<(ostream &os, const multiset<T> &v)
+{
+    os << "[";
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if (it != v.begin())
+            os << ", ";
+        os << *it;
+    }
+    return os << "]";
+}
+
+template <typename F, typename S>
+ostream &operator<<(ostream &os, const map<F, S> &v)
+{
+    os << "[";
+    for (auto it = v.begin(); it != v.end(); ++it)
+    {
+        if (it != v.begin())
+            os << ", ";
+        os << it->first << " = " << it->second;
+    }
+    return os << "]";
+}
+
+#define dbg(args...)            \
+    do                          \
+    {                           \
+        cerr << #args << " : "; \
+        faltu(args);            \
+    } while (0)
+
+void faltu()
+{
+    cerr << endl;
+}
+
+template <typename T>
+void faltu(T a[], int n)
+{
+    for (int i = 0; i < n; ++i)
+        cerr << a[i] << ' ';
+    cerr << endl;
+}
+
+template <typename T, typename... hello>
+void faltu(T arg, const hello &...rest)
+{
+    cerr << arg << ' ';
+    faltu(rest...);
+}
+
+// int min_dist_vertx(vector<int>&dist,vector<bool>&vis,int v){
+//     int minval=INF,minidx=-1;
+//     for (int i = 1; i <=v; i++)
+//     {
+//         if(!vis[i] && dist[i]<minval){
+//             minval=dist[i];
+//             minidx=i;
+//         }
+//     }
+//     return minidx;
+    
+// }
+void Dijkstra(const vector<vector<pair<int,int>>>& adj, int V, int src ){
+     vector<ll> dist(V+1, infLL);
+    dist[src] = 0;
+
+    // min-heap of (dist, vertex)
+    priority_queue<pair<ll,int>,
+                   vector<pair<ll,int>>,
+                   greater<>> pq;
+    pq.push({0, src});
+
+    while (!pq.empty()) {
+        auto [d, u] = pq.top(); pq.pop();
+        if (d > dist[u]) continue;  // stale entry
+
+        for (auto& [v, w] : adj[u]) {
+            if (d + w < dist[v]) {
+                dist[v] = d + w;
+                pq.push({dist[v], v});
+            }
+        }
+    }
+
+    for (int i = 1; i <= V; ++i) {
+        cout << dist[i]<< " ";
+    }
+    cout<<endl;
+}
+void solve(){
+    int n,m;cin>>n>>m;
+     vector<vector<pair<int,int>>> adj(n+1);
+    for (int i = 0; i < m; i++)
+    {
+        int v1,v2,w;cin>>v1>>v2>>w;
+        adj[v1].push_back({v2, w});
+    }
+    Dijkstra(adj,n,1);
+    
+    
+}
+int main()
+{
+    optimize();
+    int tc = 1;
+    //cin >> tc;
+    while (tc--)
+    {
+       solve();
+    }
+
+    return 0;
+}   
