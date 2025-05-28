964add02e96fd3c28f76fe3c1a12644f221b6cc5
anas20023 <anasibnebelal400@gmail.com>
2025-05-28 19:59:59 +0600

    Remove redundant comments in SieveOfEratosthenes function

--- seive.cpp
+++ seive.cpp
@@ -12,11 +12,6 @@ void SieveOfEratosthenes(int n)
 
 
         if (prime[p] == true) {
-            
-            // Update all multiples of p greater than or
-            // equal to the square of it numbers which are
-            // multiple of p and are less than p^2 are
-            // already been marked.
             for (int i = p * p; i <= n; i += p)
                 prime[i] = false;
         }
